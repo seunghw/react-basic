a6f283b8a825e6c618712a92a831e638f9096d3b
seunghw <ysp1307@naver.com>
2021-02-24 21:07:28 +0900

    0224

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import "./App.css";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
@@ -10,8 +13,12 @@ class App extends Component {
     {
       /* 컴포넌트가 실행될 때 render함수보다 먼저 쓰면서 초기화시켜주고 싶으면 이 안에 쓰면 됨 */
     }
+    this.max_content_id = 3;
     this.state = {
+      mode: "welcome",
+      selected_content_id: 2,
       subject: { title: "WEB", sub: "World Wide Web" },
+      welcome: { title: "Welcome", desc: "Heelo. React!!" },
       contents: [
         { id: 1, title: "HTML", desc: "HTML is for information" },
         { id: 2, title: "CSS", desc: "CSS is for design" },
@@ -20,19 +27,123 @@ class App extends Component {
     };
   }
 
+  getReadContent() {
+    var i = 0;
+    while (i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if (data.id === this.state.selected_content_id) {
+        return data;
+        break;
+      }
+      i = i + 1;
+    }
+  }
+
+  getContent() {
+    var _title,
+      _desc,
+      _article = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === "read") {
+      var _content = this.getReadContent();
+      _article = (
+        <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+      );
+    } else if (this.state.mode === "create") {
+      _article = (
+        <CreateContent
+          onSubmit={function (_title, _desc) {
+            this.max_content_id = this.max_content_id + 1;
+            var _contents = Array.from(this.state.contents);
+            _contents.push({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: _contents,
+              mode: "read",
+              selected_content_id: this.max_content_id,
+            });
+          }.bind(this)}
+        ></CreateContent>
+      );
+    } else if (this.state.mode === "update") {
+      _content = this.getReadContent();
+      _article = (
+        <UpdateContent
+          data={_content}
+          onSubmit={function (_id, _title, _desc) {
+            var _contents = Array.from(this.state.contents);
+            var i = 0;
+            while (i < _contents.length) {
+              if (_contents[i].id === _id) {
+                _contents[i] = { id: _id, title: _title, desc: _desc };
+                break;
+              }
+              i = i + 1;
+            }
+            this.setState({
+              contents: _contents,
+              mode: "read",
+            });
+          }.bind(this)}
+        ></UpdateContent>
+      );
+    }
+    return _article;
+  }
+
   render() {
+    console.log("App render");
     return (
       <div className="App">
         <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: "welcome" });
+          }.bind(this)}
         ></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is Hypertext Markup Language."
-        ></Content>
+        <TOC
+          onChangePage={function (id) {
+            this.setState({
+              mode: "read",
+              selected_content_id: Number(id),
+            });
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
+        <Control
+          onChangeMode={function (_mode) {
+            if (_mode === "delete") {
+              if (window.confirm("really?")) {
+                var _contents = Array.from(this.state.contents);
+                var i = 0;
+                while (i < _contents.length) {
+                  if (_contents[i].id === this.state.selected_content_id) {
+                    _contents.splice(i, 1);
+                    break;
+                  }
+                  i = i + 1;
+                }
+                this.setState({
+                  mode: "welcome",
+                  contents: _contents,
+                });
+                alert("deleted!");
+              }
+            } else {
+              this.setState({
+                mode: _mode,
+              });
+            }
+          }.bind(this)}
+        ></Control>
+        {this.getContent()}
       </div>
     );
   }
